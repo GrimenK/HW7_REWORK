6d22b86817364a2622d21292aeb983dd56c06c45
Oleh <oleh@Oleh.local>
2022-08-16 17:22:33 +0300

    set task B done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,41 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import setTask.CustomEntity;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Main {
-    public static Logger logger = LogManager.getLogger(Main.class);
+    public static Logger logger;
     
     public static void main(String[] args) {
+        logger = LogManager.getLogger(Main.class);
 
+//--TASK B--//
+        Set<CustomEntity> entitySet = new HashSet<>();
+        generateTestEntities(entitySet, 5);
+
+        for (CustomEntity entity: entitySet
+        ) {
+            System.out.println(entity);
+        }
+
+        logger.info("This is sorted set from previous HashSet: \n");
+        Set<CustomEntity> sortedEntitySet = new TreeSet<>(Comparator.comparingInt(CustomEntity::getNumberAttribute));
+        sortedEntitySet.addAll(entitySet);
+        for (CustomEntity entity: sortedEntitySet
+        ) {
+            System.out.println(entity);
+        }
+    }
+
+    public static void generateTestEntities(Set<CustomEntity> entitySet, int quantity){
+        for (int i =0; i<quantity; i++){
+            entitySet.add(new CustomEntity("Entity № " + i, ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE)));
+        }
     }
 }
